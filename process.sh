34066d7aaf2fa701888accfd3d0b864cbefa6e91
Barry Smith <barry.smithjr@gmail.com>
2023-01-29 14:38:59 -0600

    Create process.sh

---
+++ process.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xcaddy build --with github.com/caddy-dns/cloudflare --with github.com/kirsch33/realip --with github.com/mholt/caddy-dynamicdns --with github.com/caddyserver/transform-encoder
